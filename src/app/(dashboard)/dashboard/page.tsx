7542da40d1d3292f15821f09fb03005e3f3db7ff
senna-lang <asrsena3302@gmail.com>
2024-05-26 22:07:03 +0900

    add dashboard nav

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,23 @@
+import DashboardHeader from "@/components/DashboardHeader";
+import DashboardShell from "@/components/DashboardShell";
+import PostCreateButton from "@/components/PostCreateButton";
+import PostItem from "@/components/PostItem";
+import { allPosts } from "contentlayer/generated";
+
 export default function Dashboard() {
+  const posts = allPosts;
   return (
-   <div>hello</div>
+    <DashboardShell>
+      <DashboardHeader heading="記事投稿" text="記事の投稿と管理">
+        <PostCreateButton />
+      </DashboardHeader>
+      {posts.map(post => (
+        <div className="flex flex-col space-y-1 ">
+          <div className=" border border-muted-foreground border-dashed rounded-md">
+            <PostItem key={post.slug} post={post} />
+          </div>
+        </div>
+      ))}
+    </DashboardShell>
   );
 }
